bd36f37a2a1f71e77e30d15bc6c02e9274d58560
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-02 18:08:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Ku Ku")
+print(10, "Python", True)
+
+name = input("Enter your name: ")
+
+print(dir(name))
+
+print(name)
+
+print(name.capitalize())
+
+print(name.upper())
+
+print(name.casefold())
