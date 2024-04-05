8ea1e0f28257dededa870cd0f0863769b2949b80
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-05 22:08:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def sum_nums(a, b):
-    sum = a + b
-    return sum
-    print("Line is not executed")
+import datetime
 
-
-first_sum = sum_nums(10, 5)
-print(first_sum)
-
-print(sum_nums(50.5, 20))
-
-print(None)
+print(datetime.MAXYEAR)
