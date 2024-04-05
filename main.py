4cc4980ac0ef368ce19a07e759314254505c3932
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-05 21:52:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def sum_nums(a, b):
     sum = a + b
     return sum
+    print("Line is not executed")
 
 
 first_sum = sum_nums(10, 5)
 print(first_sum)
 
 print(sum_nums(50.5, 20))
+
+print(None)
