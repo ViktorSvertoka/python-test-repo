1879e8ae6cc16c183cf2215414991c69075a330d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-06 17:59:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-my_variable = 10
+my_number = 777  # int
 
-my_variable = "Inna"
+print(my_number)
 
-my_variable = True
+my_string = "Inna"  # str
 
-print(my_variable)
+print(my_string)
+
+my_boolean = False  # bool
+
+print(my_boolean)
+
+my_list = [1, 2, 4, 6]  # list
+
+print(my_list)
+
+my_dictionary = {"name": "Ivan", "age": 30, "salary": 4000}  # dictionary
+
+print(my_dictionary)
