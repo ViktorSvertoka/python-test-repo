714c8101d4b21eb1f8dedae295284cf6c9d388cc
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-06 18:10:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-my_number = 777  # int
+def print_name(name):
+    print(name)
 
-print(my_number)
 
-my_string = "Inna"  # str
+print_name("Inna")
 
-print(my_string)
+print_name = 15
 
-my_boolean = False  # bool
-
-print(my_boolean)
-
-my_list = [1, 2, 4, 6]  # list
-
-print(my_list)
-
-my_dictionary = {"name": "Ivan", "age": 30, "salary": 4000}  # dictionary
-
-print(my_dictionary)
+print_name("Inna")
