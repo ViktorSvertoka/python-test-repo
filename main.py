cb5eec545f8e755651ea6d58c3acaff086e9a190
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-07 13:08:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-average_price = 17.25
+average_price = 28.75
+price = int(average_price)
 
-print(average_price)  # 17.25
+print(price)  # 28
+print(type(price))  # <class 'int'>
 
-print(type(average_price))  # <class 'float'>
+str_temperature = "14.5"
+temperature = float(str_temperature)
+
+print(temperature)  # 14.5
+print(type(temperature))  # <class 'float'>
