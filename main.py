530d4b905faaf48825ea8963fdd297f706dd100f
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-07 12:57:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-base = 5
-power = 3
+one_million = 1_000_000
 
-print(pow(base, power))  # 125 or 5**3
+print(one_million)  # 1000000
+
+my_number = 3_427
+
+print(my_number)  # 3427
+
+input_str = input("Enter any number: ")
+
+input_int = int(input_str)
+
+print(input_int)
+
+print(type(input_int))
