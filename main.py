8809fd1d448db37485db653047e9e6bc9179eb05
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-07 13:04:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-one_million = 1_000_000
+average_price = 17.25
 
-print(one_million)  # 1000000
+print(average_price)  # 17.25
 
-my_number = 3_427
-
-print(my_number)  # 3427
-
-input_str = input("Enter any number: ")
-
-input_int = int(input_str)
-
-print(input_int)
-
-print(type(input_int))
+print(type(average_price))  # <class 'float'>
