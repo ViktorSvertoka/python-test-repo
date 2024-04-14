63326f5a5deabb8b06b0e8cd47fe2887374de14b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-14 21:08:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 n = 5000
-
 hours = n // (60 * 60)
 minutes = (n - hours * 60 * 60) // 60
 seconds = n - hours * 60 * 60 - minutes * 60
+
+print(n)
+print(hours)
+print(minutes)
+print(seconds)
+print(f"{hours} : {minutes} : {seconds}")
