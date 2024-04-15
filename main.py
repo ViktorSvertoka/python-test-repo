c0e2c57bc06b08964a05234d8cb20340b273c633
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-15 21:44:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,81 @@
-# Встановлюємо ціни на продукти
-price_per_croissant = 1.04
-price_per_glass = 0.34
-price_per_coffee_pack = 4.42
+my_list = [1, 2, 3, 4, 2, 2, 5, 2]
+count_2 = my_list.count(2)
+print(count_2)  # Виведе 4, оскільки число 2 зустрічається 4 рази
 
-# Кількість кожного продукту
-num_croissants = int(input("Введіть кількість круасанів: "))
-num_glasses = int(input("Введіть кількість склянок: "))
-num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
 
-# Обчислення загальної вартості
-total_cost = (
-    num_croissants * price_per_croissant
-    + num_glasses * price_per_glass
-    + num_coffee_packs * price_per_coffee_pack
-)
+my_list = [1, 2, 3, 4, 5]
+print(len(my_list))
 
-# Визначаємо кількість повних доларів і центів
-total_dollars = int(total_cost)
-total_cents = int(total_cost * 100)
 
-# Вивід результату
-print(f"Загальна вартість у повних доларах: {total_dollars} доларів")
-print(f"Загальна вартість у центах: {total_cents} центів")
+nums = [3, 1, 4, 1, 5, 9, 2]
+nums.sort()
+print(nums)  # Виведе [1, 1, 2, 3, 4, 5, 9]
+
+
+nums.sort(reverse=True)
+print(nums)  # Виведе [9, 5, 4, 3, 2, 1, 1]
+
+
+words = ["banana", "apple", "cherry"]
+sorted_words = sorted(words, key=len)
+print(sorted_words)  # Виведе ['apple', 'banana', 'cherry']
+
+
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+print(my_dict["name"])  # Виведе 'Alice'
+
+
+my_dict["age"] = 26  # Змінює вік на 26
+my_dict["email"] = "alice@example.com"  # Додає нову пару ключ-значення
+print(my_dict)
+
+
+del my_dict["age"]
+print(my_dict)
+
+
+print("name" in my_dict)
+print("age" in my_dict)
+
+
+my_dict = {"name": "Alice", "age": 25}
+age = my_dict.get("age")  # Поверне 25
+gender = my_dict.get("gender")  # Поверне None, оскільки "gender" немає в словнику
+
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a.intersection(b))  # {3}
+print(a & b)  # {3}
+
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a.difference(b))  # {1, 2}
+print(a - b)  # {1, 2}
+
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a.symmetric_difference(b))  # {1, 2, 4, 5}
+print(a ^ b)  # {1, 2, 4, 5}
+
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a.union(b))  # {1, 2, 3, 4, 5}
+print(a | b)  # {1, 2, 3, 4, 5}
+
+
+a = frozenset([1, 2, 3])
+b = frozenset([3, 4, 5])
+
+union = a | b  # Об'єднання множин
+intersection = a & b  # Перетин множин
+difference = a - b  # Різниця множин
+symmetric_difference = a ^ b  # Симетрична різниця
+
+print(union)  # frozenset({1, 2, 3, 4, 5})
+print(intersection)  # frozenset({3})
+print(difference)  # frozenset({1, 2})
+print(symmetric_difference)  # frozenset({1, 2, 4, 5})
