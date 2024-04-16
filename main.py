eda85ea5ce6bf34e8126027785319ca123bce883
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-16 21:51:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,15 @@
-my_list = [1, 2, 3, 4, 2, 2, 5, 2]
-count_2 = my_list.count(2)
-print(count_2)  # Виведе 4, оскільки число 2 зустрічається 4 рази
+def convert_camel_to_snake(name):
+    res = ""
+    for char in name:
+        if char.isupper():
+            res += "_" + char.lower()
+        else:
+            res += char
+    return res
 
 
-my_list = [1, 2, 3, 4, 5]
-print(len(my_list))
+s = "geeksForGeeks"
 
+snaked_s = convert_camel_to_snake(s)
 
-nums = [3, 1, 4, 1, 5, 9, 2]
-nums.sort()
-print(nums)  # Виведе [1, 1, 2, 3, 4, 5, 9]
-
-
-nums.sort(reverse=True)
-print(nums)  # Виведе [9, 5, 4, 3, 2, 1, 1]
-
-
-words = ["banana", "apple", "cherry"]
-sorted_words = sorted(words, key=len)
-print(sorted_words)  # Виведе ['apple', 'banana', 'cherry']
-
-
-my_dict = {"name": "Alice", "age": 25, "city": "New York"}
-print(my_dict["name"])  # Виведе 'Alice'
-
-
-my_dict["age"] = 26  # Змінює вік на 26
-my_dict["email"] = "alice@example.com"  # Додає нову пару ключ-значення
-print(my_dict)
-
-
-del my_dict["age"]
-print(my_dict)
-
-
-print("name" in my_dict)
-print("age" in my_dict)
-
-
-my_dict = {"name": "Alice", "age": 25}
-age = my_dict.get("age")  # Поверне 25
-gender = my_dict.get("gender")  # Поверне None, оскільки "gender" немає в словнику
-
-
-a = {1, 2, 3}
-b = {3, 4, 5}
-print(a.intersection(b))  # {3}
-print(a & b)  # {3}
-
-
-a = {1, 2, 3}
-b = {3, 4, 5}
-print(a.difference(b))  # {1, 2}
-print(a - b)  # {1, 2}
-
-
-a = {1, 2, 3}
-b = {3, 4, 5}
-print(a.symmetric_difference(b))  # {1, 2, 4, 5}
-print(a ^ b)  # {1, 2, 4, 5}
-
-
-a = {1, 2, 3}
-b = {3, 4, 5}
-print(a.union(b))  # {1, 2, 3, 4, 5}
-print(a | b)  # {1, 2, 3, 4, 5}
-
-
-a = frozenset([1, 2, 3])
-b = frozenset([3, 4, 5])
-
-union = a | b  # Об'єднання множин
-intersection = a & b  # Перетин множин
-difference = a - b  # Різниця множин
-symmetric_difference = a ^ b  # Симетрична різниця
-
-print(union)  # frozenset({1, 2, 3, 4, 5})
-print(intersection)  # frozenset({3})
-print(difference)  # frozenset({1, 2})
-print(symmetric_difference)  # frozenset({1, 2, 4, 5})
+print(snaked_s)
