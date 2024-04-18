0b1dbd32eefdfbb749b9d0a69cb24654c79ad20b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-18 22:44:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+for i in range(0, 10, 2):
+    print(i)
+
+
 some_list = ["apple", "banana", "cherry"]
 for index, value in enumerate(some_list):
     print(index, value)
+
+
+list1 = ["зелене", "стигла", "червоний"]
+list2 = ["яблуко", "вишня", "томат"]
+for number, letter in zip(list1, list2):
+    print(number, letter)
+
+
+age = input("How old are you? ")
+try:
+    age = int(age)
+    if age >= 18:
+        print("You are adult.")
+    else:
+        print("You are infant")
+except ValueError:
+    print(f"{age} is not a number")
