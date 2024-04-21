24b88a53bc4a975d6d3947770270e6373d03503c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-22 00:05:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,116 @@
-import re
+from datetime import datetime
 
-text = "Вивчення Python може бути веселим."
-pattern = r"в\w*м"
-match = re.search(pattern, text, re.IGNORECASE)
+current_datetime = datetime.now()
 
-if match:
-    print("Знайдено:", match.group())
+print(current_datetime)
+print(current_datetime.year)
+print(current_datetime.month)
+print(current_datetime.day)
+print(current_datetime.hour)
+print(current_datetime.minute)
+print(current_datetime.second)
+print(current_datetime.microsecond)
+print(current_datetime.tzinfo)
 
 
-text = "Моя електронна адреса: example@example.com"
-pattern = r"\w+@\w+\.\w+"
-match = re.search(pattern, text)
+from datetime import timedelta
 
-if match:
-    print("Електронна адреса:", match.group())
+delta = timedelta(
+    days=50,
+    seconds=27,
+    microseconds=10,
+    milliseconds=29000,
+    minutes=5,
+    hours=8,
+    weeks=2,
+)
+print(delta)
+
+
+from datetime import datetime, timedelta
+
+now = datetime.now()
+future_date = now + timedelta(days=10)  # Додаємо 10 днів до поточної дати
+print(future_date)
+
+
+from datetime import datetime
+
+# Встановлення дати спалення Москви Наполеоном (14 вересня 1812 року)
+napoleon_burns_moscow = datetime(year=1812, month=9, day=14)
+
+# Поточна дата
+current_date = datetime.now()
+
+# Розрахунок кількості днів
+days_since = current_date.toordinal() - napoleon_burns_moscow.toordinal()
+print(days_since)
+
+
+from datetime import datetime
+
+now = datetime.now()
+
+# Форматування дати і часу
+formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+print(formatted_date)
+
+# Форматування лише дати
+formatted_date_only = now.strftime("%A, %d %B %Y")
+print(formatted_date_only)
+
+# Форматування лише часу
+formatted_time_only = now.strftime("%I:%M %p")
+print(formatted_time_only)
+
+# Форматування лише дати
+formatted_date_only = now.strftime("%d.%m.%Y")
+print(formatted_date_only)
+
+
+from datetime import datetime
+
+# Припустимо, у нас є дата у вигляді рядка
+date_string = "2023.03.14"
+
+# Перетворення рядка в об'єкт datetime
+datetime_object = datetime.strptime(date_string, "%Y.%m.%d")
+print(datetime_object)  # Виведе об'єкт datetime, що відповідає вказаній даті та часу
+
+
+from datetime import datetime
+
+# Поточна дата та час
+now = datetime.now()
+
+# Конвертація у формат ISO 8601
+iso_format = now.isoformat()
+print(iso_format)
+
+
+from datetime import datetime
+
+# Поточна дата та час
+now = datetime.now()
+
+# Конвертація у формат ISO 8601
+iso_format = now.isoformat()
+print(iso_format)
+
+
+from datetime import datetime, timezone
+
+local_now = datetime.now()
+utc_now = datetime.now(timezone.utc)
+
+print(local_now)
+print(utc_now)  # Виведе поточний час в UTC
+
+
+import random
+
+cards = ["Туз", "Король", "Дама", "Валет", "10", "9", "8", "7", "6"]
+
+random.shuffle(cards)
+
+print(f"Перемішана колода: {cards}")
