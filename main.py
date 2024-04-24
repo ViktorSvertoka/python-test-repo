6af1dff9c4b5975f18be9ad381b0516b3d434a2c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-24 20:46:59 +0300

    Feat(Files) Added files

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
-fh = open("text.txt", "w")
-try:
-    # Виконання операцій з файлом
-    fh.write("Some data")
-finally:
-    # Закриття файлу в блоку finally гарантує, що файл закриється навіть у разі помилки
-    fh.close()
+with open("raw_data.bin", "wb") as fh:
+    fh.write(b"Hello world!")
 
 
-with open("text.txt", "w") as fh:
-    # Виконання операцій з файлом
-    fh.write("Some data")
-# Файл автоматично закриється після виходу з блоку with
+s = b"Hello!"
+print(s[1])  # Виведе: 101 (це ASCII-код символу 'e')
 
 
-with open("test.txt", "w") as fh:
-    fh.write("first line\nsecond line\nthird line")
+byte_str = "some text".encode()
+print(byte_str)
 
-with open("test.txt", "r") as fh:
-    lines = [el.strip() for el in fh.readlines()]
 
-print(lines)
+# Перетворення списку чисел у байт-рядок
+numbers = [0, 128, 255]
+byte_numbers = bytes(numbers)
+print(byte_numbers)  # Виведе байтове представлення чисел
+
+
+for num in [127, 255, 156]:
+    print(hex(num))
+
+
+print(ord("a"))
+
+
+print(chr(128))
+
+
+s = "Привіт!"
+
+utf8 = s.encode()
+print(f"UTF-8: {utf8}")
+
+utf16 = s.encode("utf-16")
+print(f"UTF-16: {utf16}")
+
+cp1251 = s.encode("cp1251")
+print(f"CP-1251: {cp1251}")
+
+s_from_utf16 = utf16.decode("utf-16")
+print(s_from_utf16 == s)
+
+
+print(b"Hello world!".decode("utf-16"))
+
+
+# Відкриття текстового файлу з явним вказівкам UTF-8 кодування
+with open("example.txt", "r", encoding="utf-8") as file:
+    content = file.read()
+    print(content)
