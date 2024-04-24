f65fc5e8b5ae573d9aa10cbfa2682cb12687b7eb
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-24 20:09:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-fh = open("test.txt", "w")
-fh.write("first line\nsecond line\nthird line")
-fh.close()
+fh = open("text.txt", "w")
+try:
+    # Виконання операцій з файлом
+    fh.write("Some data")
+finally:
+    # Закриття файлу в блоку finally гарантує, що файл закриється навіть у разі помилки
+    fh.close()
 
-fh = open("test.txt", "r")
-lines = [el.strip() for el in fh.readlines()]
+
+with open("text.txt", "w") as fh:
+    # Виконання операцій з файлом
+    fh.write("Some data")
+# Файл автоматично закриється після виходу з блоку with
+
+
+with open("test.txt", "w") as fh:
+    fh.write("first line\nsecond line\nthird line")
+
+with open("test.txt", "r") as fh:
+    lines = [el.strip() for el in fh.readlines()]
+
 print(lines)
-
-fh.close()
