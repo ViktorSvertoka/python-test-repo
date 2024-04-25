682a45e6a3e11a9958b91155961ac54bcbfba58d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-25 22:11:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
-with open("raw_data.bin", "wb") as fh:
-    fh.write(b"Hello world!")
+byte_array = bytearray(b"Kill Bill")
+byte_array[0] = ord("B")
+byte_array[5] = ord("K")
+print(byte_array)
 
 
-s = b"Hello!"
-print(s[1])  # Виведе: 101 (це ASCII-код символу 'e')
+string1 = "Hello World"
+string2 = "hello world"
+if string1.lower() == string2.lower():
+    print("Рядки однакові")
+else:
+    print("Рядки різні")
 
 
-byte_str = "some text".encode()
-print(byte_str)
+text = "Python Programming"
+print(text.casefold())
 
 
-# Перетворення списку чисел у байт-рядок
-numbers = [0, 128, 255]
-byte_numbers = bytes(numbers)
-print(byte_numbers)  # Виведе байтове представлення чисел
+german_word = "straße"  # В нижньому регістрі
+search_word = "STRASSE"  # В верхньому регістрі
 
+# Порівняння за допомогою lower()
+lower_comparison = german_word.lower() == search_word.lower()
 
-for num in [127, 255, 156]:
-    print(hex(num))
+# Порівняння за допомогою casefold()
+casefold_comparison = german_word.casefold() == search_word.casefold()
 
-
-print(ord("a"))
-
-
-print(chr(128))
-
-
-s = "Привіт!"
-
-utf8 = s.encode()
-print(f"UTF-8: {utf8}")
-
-utf16 = s.encode("utf-16")
-print(f"UTF-16: {utf16}")
-
-cp1251 = s.encode("cp1251")
-print(f"CP-1251: {cp1251}")
-
-s_from_utf16 = utf16.decode("utf-16")
-print(s_from_utf16 == s)
-
-
-print(b"Hello world!".decode("utf-16"))
-
-
-# Відкриття текстового файлу з явним вказівкам UTF-8 кодування
-with open("example.txt", "r", encoding="utf-8") as file:
-    content = file.read()
-    print(content)
+print(f"Порівняння з lower(): {lower_comparison}")
+print(f"Порівняння з casefold(): {casefold_comparison}")
