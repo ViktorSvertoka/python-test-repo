bc3fecf6c5e39f1d77222b340fe168c5c052d3f0
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-04-27 23:21:56 +0300

    Feat(Files) Added task 1

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
+import shutil
 from pathlib import Path
 
-# Створення об'єкту Path для файлу
-file_path = Path("example.txt")
+# Вихідний і цільовий файли
+source = Path("/path/to/source/file.txt")
+destination = Path("/path/to/destination/file.txt")
 
-# Читання тексту з файлу
-text = file_path.read_text(encoding="utf-8")
-print(text)
-
-
-from pathlib import Path
-
-# Створення об'єкту Path для бінарного файлу
-file_path = Path("example.bin")
-
-# Бінарні дані для запису
-data = b"Python is great!"
-
-# Запис байтів у файл
-file_path.write_bytes(data)
-
-
-from pathlib import Path
-
-# Створення об'єкту Path для бінарного файлу
-file_path = Path("example.bin")
-
-# Читання байтів з файлу
-binary_data = file_path.read_bytes()
-print(binary_data)
+# Копіювання файла
+shutil.copy(source, destination)
