d3dbf2e7a0c96fec97856d9f2fdead889e5f9ca1
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-01 23:21:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,89 @@
-student_marks = [4, 2, 4, 6, 7, 4, 2, 3, 4, 5, 6, 6, 7, 1, 1, 1, 3, 5]
-mark_counts = {}
-for mark in student_marks:
-    if mark in mark_counts:
-        mark_counts[mark] += 1
+# Створення стеку
+def create_stack():
+    return []
+
+
+# Перевірка на порожнечу
+def is_empty(stack):
+    return len(stack) == 0
+
+
+# Додавання елементу
+def push(stack, item):
+    stack.append(item)
+
+
+# Вилучення елементу
+def pop(stack):
+    if not is_empty(stack):
+        return stack.pop()
     else:
-        mark_counts[mark] = 1
-
-print(mark_counts)
+        print("Стек порожній")
 
 
-import collections
+# Перегляд верхнього елемента
+def peek(stack):
+    if not is_empty(stack):
+        return stack[-1]
+    else:
+        print("Стек порожній")
 
-student_marks = [4, 2, 4, 6, 7, 4, 2, 3, 4, 5, 6, 6, 7, 1, 1, 1, 3, 5]
-mark_counts = collections.Counter(student_marks)
-print(mark_counts)
+
+stack = create_stack()
+push(stack, "a")
+push(stack, "b")
+push(stack, "c")
+
+
+print(peek(stack))  # Виведе 'c'
+print(pop(stack))  # Виведе 'c'
+
+
+from collections import deque
+
+# Створення черги
+queue = deque()
+
+# Enqueue: Додавання елементів
+queue.append("a")
+queue.append("b")
+queue.append("c")
+
+print("Черга після додавання елементів:", list(queue))
+
+# Dequeue: Видалення елемента
+print("Видалений елемент:", queue.popleft())
+
+print("Черга після видалення елемента:", list(queue))
+
+# Peek: Перегляд першого елемента
+print("Перший елемент у черзі:", queue[0])
+
+# IsEmpty: Перевірка на порожнечу
+print("Чи черга порожня:", len(queue) == 0)
+
+# Size: Розмір черги
+print("Розмір черги:", len(queue))
+
+
+from collections import deque
+
+# Створення пустої двосторонньої черги
+d = deque()
+
+# Додаємо елементи в чергу
+d.append("middle")  # Додаємо 'middle' в кінець черги
+d.append("last")  # Додаємо 'last' в кінець черги
+d.appendleft("first")  # Додаємо 'first' на початок черги
+
+# Виведення поточного стану черги
+print("Черга після додавання елементів:", list(d))
+
+# Видалення та виведення останнього елемента (з правого кінця)
+print("Видалений останній елемент:", d.pop())
+
+# Видалення та виведення першого елемента (з лівого кінця)
+print("Видалений перший елемент:", d.popleft())
+
+# Виведення поточного стану черги після видалення елементів
+print("Черга після видалення елементів:", list(d))
