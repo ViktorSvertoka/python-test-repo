12d316656765a7bba6f3fca9d14d5098f07dd6ab
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-01 23:39:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-import decimal
-from decimal import Decimal
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
 
-number = Decimal("1.45")
 
-# Округлення за замовчуванням до одного десяткового знаку
-print("Округлення за замовчуванням ROUND_HALF_EVEN:", number.quantize(Decimal("0.0")))
+gen = my_generator()
 
-# Округлення вверх при нічиї (ROUND_HALF_UP)
-print(
-    "Округлення вгору ROUND_HALF_UP:",
-    number.quantize(Decimal("0.0"), rounding=decimal.ROUND_HALF_UP),
-)
-
-# Округлення вниз (ROUND_FLOOR)
-print(
-    "Округлення вниз ROUND_FLOOR:",
-    number.quantize(Decimal("0.0"), rounding=decimal.ROUND_FLOOR),
-)
-
-# Округлення вверх (ROUND_CEILING)
-print(
-    "Округлення вгору ROUND_CEILING:",
-    number.quantize(Decimal("0.0"), rounding=decimal.ROUND_CEILING),
-)
-
-# Округлення до трьох десяткових знаків за замовчуванням
-print(
-    "Округлення до трьох десяткових знаків:",
-    Decimal("3.14159").quantize(Decimal("0.000")),
-)
+# Використання next()
+print(next(gen))  # Виведе 1
+print(next(gen))  # Виведе 2
+print(next(gen))  # Виведе 3
