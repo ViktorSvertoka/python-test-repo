1ddbf109e790b763d806bec1d892f50ec0ceda8d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-01 23:33:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,31 @@
-# Створення стеку
-def create_stack():
-    return []
+import decimal
+from decimal import Decimal
 
+number = Decimal("1.45")
 
-# Перевірка на порожнечу
-def is_empty(stack):
-    return len(stack) == 0
+# Округлення за замовчуванням до одного десяткового знаку
+print("Округлення за замовчуванням ROUND_HALF_EVEN:", number.quantize(Decimal("0.0")))
 
+# Округлення вверх при нічиї (ROUND_HALF_UP)
+print(
+    "Округлення вгору ROUND_HALF_UP:",
+    number.quantize(Decimal("0.0"), rounding=decimal.ROUND_HALF_UP),
+)
 
-# Додавання елементу
-def push(stack, item):
-    stack.append(item)
+# Округлення вниз (ROUND_FLOOR)
+print(
+    "Округлення вниз ROUND_FLOOR:",
+    number.quantize(Decimal("0.0"), rounding=decimal.ROUND_FLOOR),
+)
 
+# Округлення вверх (ROUND_CEILING)
+print(
+    "Округлення вгору ROUND_CEILING:",
+    number.quantize(Decimal("0.0"), rounding=decimal.ROUND_CEILING),
+)
 
-# Вилучення елементу
-def pop(stack):
-    if not is_empty(stack):
-        return stack.pop()
-    else:
-        print("Стек порожній")
-
-
-# Перегляд верхнього елемента
-def peek(stack):
-    if not is_empty(stack):
-        return stack[-1]
-    else:
-        print("Стек порожній")
-
-
-stack = create_stack()
-push(stack, "a")
-push(stack, "b")
-push(stack, "c")
-
-
-print(peek(stack))  # Виведе 'c'
-print(pop(stack))  # Виведе 'c'
-
-
-from collections import deque
-
-# Створення черги
-queue = deque()
-
-# Enqueue: Додавання елементів
-queue.append("a")
-queue.append("b")
-queue.append("c")
-
-print("Черга після додавання елементів:", list(queue))
-
-# Dequeue: Видалення елемента
-print("Видалений елемент:", queue.popleft())
-
-print("Черга після видалення елемента:", list(queue))
-
-# Peek: Перегляд першого елемента
-print("Перший елемент у черзі:", queue[0])
-
-# IsEmpty: Перевірка на порожнечу
-print("Чи черга порожня:", len(queue) == 0)
-
-# Size: Розмір черги
-print("Розмір черги:", len(queue))
-
-
-from collections import deque
-
-# Створення пустої двосторонньої черги
-d = deque()
-
-# Додаємо елементи в чергу
-d.append("middle")  # Додаємо 'middle' в кінець черги
-d.append("last")  # Додаємо 'last' в кінець черги
-d.appendleft("first")  # Додаємо 'first' на початок черги
-
-# Виведення поточного стану черги
-print("Черга після додавання елементів:", list(d))
-
-# Видалення та виведення останнього елемента (з правого кінця)
-print("Видалений останній елемент:", d.pop())
-
-# Видалення та виведення першого елемента (з лівого кінця)
-print("Видалений перший елемент:", d.popleft())
-
-# Виведення поточного стану черги після видалення елементів
-print("Черга після видалення елементів:", list(d))
+# Округлення до трьох десяткових знаків за замовчуванням
+print(
+    "Округлення до трьох десяткових знаків:",
+    Decimal("3.14159").quantize(Decimal("0.000")),
+)
