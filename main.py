cf979470305536451090506f8ba82359b8b60af6
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-01 23:05:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+student_marks = [4, 2, 4, 6, 7, 4, 2, 3, 4, 5, 6, 6, 7, 1, 1, 1, 3, 5]
+mark_counts = {}
+for mark in student_marks:
+    if mark in mark_counts:
+        mark_counts[mark] += 1
+    else:
+        mark_counts[mark] = 1
+
+print(mark_counts)
+
+
 import collections
 
-# Створення іменованого кортежу Person
-Person = collections.namedtuple(
-    "Person", ["first_name", "last_name", "age", "birth_place", "post_index"]
-)
-
-# Створення екземпляра Person
-person = Person("Mick", "Nitch", 35, "Boston", "01146")
-
-# Виведення різних атрибутів іменованого кортежу
-print(person.first_name)
-print(person.post_index)
-print(person.age)
-print(person[3])
+student_marks = [4, 2, 4, 6, 7, 4, 2, 3, 4, 5, 6, 6, 7, 1, 1, 1, 3, 5]
+mark_counts = collections.Counter(student_marks)
+print(mark_counts)
