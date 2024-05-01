519586a576712f7c9c2d4dcf171e86d2114c1808
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-01 22:52:29 +0300

    Test

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
-def is_palindrome(s: str) -> bool:
-    new_s = ""
+import collections
 
-    for char in s:
-        if char.isalnum():
-            new_s += char.lower()
+# Створення іменованого кортежу Person
+Person = collections.namedtuple(
+    "Person", ["first_name", "last_name", "age", "birth_place", "post_index"]
+)
 
-    s = new_s
-    length = len(s)
+# Створення екземпляра Person
+person = Person("Mick", "Nitch", 35, "Boston", "01146")
 
-    for i in range(length // 2):
-        if s[i] != s[length - i - 1]:
-            return False
-
-    return True
-
-
-# Використання функції
-print(is_palindrome("Козак з казок"))  # Виведе: True
-
-
-# KISS
-
-
-def is_palindrome(s: str) -> bool:
-    new_s = ""
-    for char in s:
-        if char.isalnum():
-            new_s += char.lower()
-
-    s = new_s
-    return s == s[::-1]
-
-
-# Використання функції
-print(is_palindrome("Козак з казок"))  # Виведе: True
+# Виведення різних атрибутів іменованого кортежу
+print(person.first_name)
+print(person.post_index)
+print(person.age)
+print(person[3])
--- test.py
+++
@@ -1,6 +0,0 @@
-from colorama import Fore, Back, Style
-
-print(Fore.RED + "Це червоний текст")
-print(Back.GREEN + "Це текст на зеленому фоні")
-print(Style.RESET_ALL)
-print("Це звичайний текст після скидання стилю")
