d9b5e08e9ce33d8bf0b1fef63edece97068611c4
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-02 22:31:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,85 @@
-def my_generator():
-    yield 1
-    yield 2
-    yield 3
+def my_function():
+    print("Hello, world!")
 
 
-gen = my_generator()
+f = my_function
+f()
 
-# Використання next()
-print(next(gen))  # Виведе 1
-print(next(gen))  # Виведе 2
-print(next(gen))  # Виведе 3
+
+from typing import Callable
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+def apply_operation(a: int, b: int, operation: Callable[[int, int], int]) -> int:
+    return operation(a, b)
+
+
+# Використання
+result_add = apply_operation(5, 3, add)
+result_multiply = apply_operation(5, 3, multiply)
+
+print(result_add, result_multiply)
+
+
+from typing import Callable
+
+
+def power(exponent: int) -> Callable[[int], int]:
+    def inner(base: int) -> int:
+        return base**exponent
+
+    return inner
+
+
+# Використання
+square = power(2)
+cube = power(3)
+
+print(square(4))
+print(cube(4))
+
+
+from typing import Callable, Dict
+
+
+# Визначення функцій
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+def power(exponent: int) -> Callable[[int], int]:
+    def inner(base: int) -> int:
+        return base**exponent
+
+    return inner
+
+
+# Використання power для створення функцій square та cube
+square = power(2)
+cube = power(3)
+
+# Словник операцій
+operations: Dict[str, Callable] = {
+    "add": add,
+    "multiply": multiply,
+    "square": square,
+    "cube": cube,
+}
+
+# Використання операцій
+result_add = operations["add"](10, 20)  # 30
+result_square = operations["square"](5)  # 25
+
+print(result_add)
+print(result_square)
