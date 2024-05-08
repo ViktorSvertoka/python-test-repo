c413cd17e30d3a3446cfd82904b7c52899496bfe
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-08 21:43:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,19 @@
-class Animal:
-    def __init__(self, nickname: str, age: int):
-        self.nickname = nickname
-        self.age = age
-
-    def make_sound(self):
-        pass
+class Duck:
+    def quack(self):
+        print("Quack, quack!")
 
 
-class Cat(Animal):
-    def make_sound(self) -> str:
-        return "Meow"
+class Person:
+    def quack(self):
+        print("I'm Quacking Like a Duck!")
 
 
-class Dog(Animal):
-
-    def __init__(self, nickname: str, age: int, breed: str):
-        super().__init__(nickname, age)  # Викликаємо конструктор базового класу
-        self.breed = breed  # Додаємо нову властивість
-
-    def make_sound(self) -> str:
-        return "Woof"
-
-    def chase_tail(self) -> str:
-        return f"{self.nickname} is chasing its tail!"
+def make_it_quack(duck):
+    duck.quack()
 
 
-class Cow(Animal):
-    def make_sound(self):
-        return "Moo"
+duck = Duck()
+person = Person()
 
-
-my_cat = Cat("Simon", 4)
-my_cow = Cow("Bessie", 3)
-
-print(my_cat.make_sound())  # Виведе "Meow"
-print(my_cow.make_sound())  # Виведе "Moo"
-
-my_dog = Dog("Rex", 5, "Golden Retriever")
-print(my_dog.make_sound())  # Виведе "Woof"
-print(my_dog.chase_tail())  # Виведе "Rex is chasing its tail!"
+make_it_quack(duck)
+make_it_quack(person)
