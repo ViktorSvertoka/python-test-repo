b6b319b8c28bde470752a86313209d9e85987b3d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-08 21:32:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,149 +1,41 @@
-class User:
-    name = "Anonymous"
-    age = 15
-
-
-user1 = User()
-print(user1.name)
-print(user1.age)
-
-user2 = User()
-user2.name = "John"
-user2.age = 90
-
-print(user2.name)
-print(user2.age)
-
-
-class Person:
-    def __init__(self, name: str, age: int):
-        self.name = name
+class Animal:
+    def __init__(self, nickname: str, age: int):
+        self.nickname = nickname
         self.age = age
 
-    def say_name(self) -> None:
-        print(f"Hi! I am {self.name} and I am {self.age} years old.")
-
-    def set_age(self, age: int) -> None:
-        self.age = age
+    def make_sound(self):
+        pass
 
 
-bob = Person("Boris", 34)
-
-bob.say_name()
-bob.set_age(25)
-bob.say_name()
+class Cat(Animal):
+    def make_sound(self) -> str:
+        return "Meow"
 
 
-class Person:
-    count = 0
+class Dog(Animal):
 
-    def __init__(self, name: str):
-        self.name = name
-        Person.count += 1
+    def __init__(self, nickname: str, age: int, breed: str):
+        super().__init__(nickname, age)  # Викликаємо конструктор базового класу
+        self.breed = breed  # Додаємо нову властивість
 
-    def how_many_persons(self):
-        print(f"Кількість людей зараз {Person.count}")
+    def make_sound(self) -> str:
+        return "Woof"
 
-
-first = Person("Boris")
-first.how_many_persons()
-second = Person("Alex")
-first.how_many_persons()
+    def chase_tail(self) -> str:
+        return f"{self.nickname} is chasing its tail!"
 
 
-class Pokemon:
-    def __init__(self, name, type, health):
-        self.name = name
-        self.type = type
-        self.health = health
-
-    def attack(self, other_pokemon):
-        print(f"{self.name} attacks {other_pokemon.name}!")
-
-    def dodge(self):
-        print(f"{self.name} dodged the attack!")
-
-    def evolve(self, new_form):
-        print(f"{self.name} is evolving into {new_form}!")
-        self.name = new_form
+class Cow(Animal):
+    def make_sound(self):
+        return "Moo"
 
 
-# Створення об'єкта Pikachu
-pikachu = Pokemon("Pikachu", "Electric", 100)
+my_cat = Cat("Simon", 4)
+my_cow = Cow("Bessie", 3)
 
-# Використання методів
-pikachu.attack(Pokemon("Charmander", "Fire", 100))
-pikachu.dodge()
-pikachu.evolve("Raichu")
+print(my_cat.make_sound())  # Виведе "Meow"
+print(my_cow.make_sound())  # Виведе "Moo"
 
-
-class Person:
-    def __init__(self, name: str, age: int, is_active: bool):
-        self.name = name
-        self.age = age
-        self._is_active = is_active
-
-    def greeting(self):
-        return f"Hi {self.name}"
-
-    def is_active(self):
-        return self._is_active
-
-    def set_active(self, active: bool):
-        self._is_active = active
-
-
-p = Person("Boris", 34, True)
-print(p.name, p.age, p.is_active())
-print(p.greeting())
-
-
-class Person:
-    def __init__(self, name: str, age: int, is_active: bool, is_admin: bool):
-        self.name = name
-        self.age = age
-        self._is_active = is_active
-        self.__is_admin = is_admin
-
-    def greeting(self):
-        return f"Hi {self.name}"
-
-    def is_active(self):
-        return self._is_active
-
-    def set_active(self, active: bool):
-        self._is_active = active
-
-
-p = Person("Boris", 34, True, False)
-print(p._Person__is_admin)
-
-
-class Person:
-    def __init__(self, name: str, age: int, is_active: bool, is_admin: bool):
-        self.name = name
-        self.age = age
-        self._is_active = is_active
-        self.__is_admin = is_admin
-
-    def greeting(self):
-        return f"Hi {self.name}"
-
-    def is_active(self):
-        return self._is_active
-
-    def set_active(self, active: bool):
-        self._is_active = active
-
-    def get_is_admin(self):
-        return self.__is_admin
-
-    def set_is_admin(self, is_admin: bool):
-        # Тут можна додати будь-яку логіку перевірки або обробки
-        self.__is_admin = is_admin
-
-
-p = Person("Boris", 34, True, False)
-print(p.get_is_admin())
-p.set_is_admin(True)
-print(p.get_is_admin())
+my_dog = Dog("Rex", 5, "Golden Retriever")
+print(my_dog.make_sound())  # Виведе "Woof"
+print(my_dog.chase_tail())  # Виведе "Rex is chasing its tail!"
