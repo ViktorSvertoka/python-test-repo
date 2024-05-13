49893707a8fda69e2cb96e431542a71f7ea0ba9c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-13 22:57:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,87 @@
-from collections import UserDict
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def area(self):
+        return self.width * self.height
 
-class MyDict(UserDict):
-    def __add__(self, other):
-        temp_dict = self.data.copy()
-        temp_dict.update(other)
-        return MyDict(temp_dict)
+    def __eq__(self, other):
+        if not isinstance(other, Rectangle):
+            return NotImplemented
+        return self.area() == other.area()
 
-    def __sub__(self, other):
-        temp_dict = self.data.copy()
-        for key in other:
-            if key in temp_dict:
-                temp_dict.pop(key)
-        return MyDict(temp_dict)
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
+    def __lt__(self, other):
+        if not isinstance(other, Rectangle):
+            return NotImplemented
+        return self.area() < other.area()
 
-if __name__ == "__main__":
-    d1 = MyDict({1: "a", 2: "b"})
-    d2 = MyDict({3: "c", 4: "d"})
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
 
-    d3 = d1 + d2
-    print(d3)
+    def __gt__(self, other):
+        if not isinstance(other, Rectangle):
+            return NotImplemented
+        return self.area() > other.area()
 
-    d4 = d3 - d2
-    print(d4)
-
-
-class ComplexNumber:
-    def __init__(self, real, imag):
-        self.real = real
-        self.imag = imag
-
-    def __add__(self, other):
-        return ComplexNumber(self.real + other.real, self.imag + other.imag)
-
-    def __sub__(self, other):
-        return ComplexNumber(self.real - other.real, self.imag - other.imag)
-
-    def __mul__(self, other):
-        real_part = self.real * other.real - self.imag * other.imag
-        imag_part = self.real * other.imag + self.imag * other.real
-        return ComplexNumber(real_part, imag_part)
-
-    def __str__(self):
-        return f"{self.real} + {self.imag}i"
+    def __ge__(self, other):
+        return self.__gt__(other) or self.__eq__(other)
 
 
 if __name__ == "__main__":
-    num1 = ComplexNumber(1, 2)
-    num2 = ComplexNumber(3, 4)
-    print(f"Сума: {num1 + num2}")
-    print(f"Різниця: {num1 - num2}")
-    print(f"Добуток: {num1 * num2}")
+    rect1 = Rectangle(5, 10)
+    rect2 = Rectangle(3, 20)
+    rect3 = Rectangle(5, 10)
+    print(f"Площа прямокутників: {rect1.area()}, {rect2.area()}, {rect3.area()}")
+    print(rect1 == rect3)  # True: площі рівні
+    print(rect1 != rect2)  # True: площі не рівні
+    print(rect1 < rect2)  # True: площа rect1  менша, ніж у rect2
+    print(rect1 <= rect3)  # True: площі рівні, тому rect1 <= rect3
+    print(rect1 > rect2)  # False: площа rect1 менша, ніж у rect2
+    print(rect1 >= rect3)  # True: площі рівні, тому rect1 >= rect3
 
 
-from collections import UserList
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+    def __eq__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return self.x == other.x and self.y == other.y
 
-class MulArray(UserList):
-    def __init__(self, *args):
-        self.data = list(args)
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
-    def __mul__(self, other):
-        return self.__scalar_mul(other)
+    def __lt__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return self.x < other.x and self.y < other.y
 
-    def __rmul__(self, other):
-        return self.__scalar_mul(other)
+    def __gt__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return self.x > other.x and self.y > other.y
 
-    def __scalar_mul(self, other):
-        result = 0
-        for i in range(min(len(self.data), len(other))):
-            result += self.data[i] * other[i]
-        return result
+    def __le__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return self.x <= other.x and self.y <= other.y
+
+    def __ge__(self, other):
+        if not isinstance(other, Point):
+            return NotImplemented
+        return self.x >= other.x and self.y >= other.y
 
 
 if __name__ == "__main__":
-    vec1 = MulArray(1, 2, 3)
-    vec2 = MulArray(3, 4, 5)
-
-    print(vec1 * vec2)
-    print(vec1 * [1, 2, 3])
-    print([1, 1, 1] * vec2)
+    print(Point(0, 0) == Point(0, 0))  # True
+    print(Point(0, 0) != Point(0, 0))  # False
+    print(Point(0, 0) < Point(1, 0))  # False
+    print(Point(0, 0) > Point(0, 1))  # False
+    print(Point(0, 2) >= Point(0, 1))  # True
+    print(Point(0, 0) <= Point(0, 0))  # True
