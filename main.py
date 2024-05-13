90c533042f862e11e6374ea805b1aa0b3a25b277
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-13 22:53:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,108 +1,83 @@
-class SimpleDict:
-    def __init__(self):
-        self.__data = {}
-
-    def __getitem__(self, key):
-        return self.__data.get(key, "Key not found")
-
-    def __setitem__(self, key, value):
-        self.__data[key] = value
+from collections import UserDict
 
 
-# Використання класу
-simple_dict = SimpleDict()
-simple_dict["name"] = "Boris"
-print(simple_dict["name"])
-print(simple_dict["age"])
+class MyDict(UserDict):
+    def __add__(self, other):
+        temp_dict = self.data.copy()
+        temp_dict.update(other)
+        return MyDict(temp_dict)
 
-
-class BoundedList:
-    def __init__(self, min_value: int, max_value: int):
-        self.min_value = min_value
-        self.max_value = max_value
-        self.__data = []
-
-    def __getitem__(self, index: int):
-        return self.__data[index]
-
-    def __setitem__(self, index: int, value: int):
-        if not (self.min_value <= value <= self.max_value):
-            raise ValueError(
-                f"Value {value} must be between {self.min_value} and {self.max_value}"
-            )
-        if index >= len(self.__data):
-            # Додати новий елемент, якщо індекс виходить за межі
-            self.__data.append(value)
-        else:
-            # Замінити існуючий елемент
-            self.__data[index] = value
-
-    def __repr__(self):
-        return f"BoundedList({self.max_value}, {self.min_value})"
-
-    def __str__(self):
-        return str(self.__data)
+    def __sub__(self, other):
+        temp_dict = self.data.copy()
+        for key in other:
+            if key in temp_dict:
+                temp_dict.pop(key)
+        return MyDict(temp_dict)
 
 
 if __name__ == "__main__":
-    temperatures = BoundedList(18, 26)
+    d1 = MyDict({1: "a", 2: "b"})
+    d2 = MyDict({3: "c", 4: "d"})
 
-    for i, el in enumerate([20, 22, 25, 27]):
-        try:
-            temperatures[i] = el
-        except ValueError as e:
-            print(e)
+    d3 = d1 + d2
+    print(d3)
 
-    print(temperatures)
+    d4 = d3 - d2
+    print(d4)
+
+
+class ComplexNumber:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __add__(self, other):
+        return ComplexNumber(self.real + other.real, self.imag + other.imag)
+
+    def __sub__(self, other):
+        return ComplexNumber(self.real - other.real, self.imag - other.imag)
+
+    def __mul__(self, other):
+        real_part = self.real * other.real - self.imag * other.imag
+        imag_part = self.real * other.imag + self.imag * other.real
+        return ComplexNumber(real_part, imag_part)
+
+    def __str__(self):
+        return f"{self.real} + {self.imag}i"
+
+
+if __name__ == "__main__":
+    num1 = ComplexNumber(1, 2)
+    num2 = ComplexNumber(3, 4)
+    print(f"Сума: {num1 + num2}")
+    print(f"Різниця: {num1 - num2}")
+    print(f"Добуток: {num1 * num2}")
 
 
 from collections import UserList
 
 
-class BoundedList(UserList):
-    def __init__(self, min_value: int, max_value: int, initial_list=None):
-        super().__init__(initial_list if initial_list is not None else [])
-        self.min_value = min_value
-        self.max_value = max_value
-        self.__validate_list()
+class MulArray(UserList):
+    def __init__(self, *args):
+        self.data = list(args)
 
-    def __validate_list(self):
-        for item in self.data:
-            self.__validate_item(item)
+    def __mul__(self, other):
+        return self.__scalar_mul(other)
 
-    def __validate_item(self, item):
-        if not (self.min_value <= item <= self.max_value):
-            raise ValueError(
-                f"Item {item} must be between {self.min_value} and {self.max_value}"
-            )
+    def __rmul__(self, other):
+        return self.__scalar_mul(other)
 
-    def append(self, item):
-        self.__validate_item(item)
-        super().append(item)
-
-    def insert(self, i, item):
-        self.__validate_item(item)
-        super().insert(i, item)
-
-    def __setitem__(self, i, item):
-        self.__validate_item(item)
-        super().__setitem__(i, item)
-
-    def __repr__(self):
-        return f"BoundedList({self.max_value}, {self.min_value})"
-
-    def __str__(self):
-        return str(self.data)
+    def __scalar_mul(self, other):
+        result = 0
+        for i in range(min(len(self.data), len(other))):
+            result += self.data[i] * other[i]
+        return result
 
 
 if __name__ == "__main__":
-    temperatures = BoundedList(18, 26, [19, 21, 22])
-    print(temperatures)
+    vec1 = MulArray(1, 2, 3)
+    vec2 = MulArray(3, 4, 5)
 
-    for el in [20, 22, 25, 27]:
-        try:
-            temperatures.append(el)
-        except ValueError as e:
-            print(e)
-
-    print(temperatures)
+    print(vec1 * vec2)
+    print(vec1 * [1, 2, 3])
+    print([1, 1, 1] * vec2)
