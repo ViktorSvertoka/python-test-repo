6794061ab11704f55cfb35002807330f71223714
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-13 23:05:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,87 +1,62 @@
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+class Person:
+    def __init__(self, age):
+        # Спочатку встановлюємо __age як None
+        self.__age = None
+        # Використовуємо сеттер для встановлення віку, що дозволяє валідацію вхідного значення
+        self.age = age
 
-    def area(self):
-        return self.width * self.height
+    @property
+    def age(self):
+        # Геттер повертає значення приватного поля
+        return self.__age
 
-    def __eq__(self, other):
-        if not isinstance(other, Rectangle):
-            return NotImplemented
-        return self.area() == other.area()
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __lt__(self, other):
-        if not isinstance(other, Rectangle):
-            return NotImplemented
-        return self.area() < other.area()
-
-    def __le__(self, other):
-        return self.__lt__(other) or self.__eq__(other)
-
-    def __gt__(self, other):
-        if not isinstance(other, Rectangle):
-            return NotImplemented
-        return self.area() > other.area()
-
-    def __ge__(self, other):
-        return self.__gt__(other) or self.__eq__(other)
+    @age.setter
+    def age(self, value):
+        if value < 0:
+            # Валідація вхідного значення
+            raise ValueError("Вік не може бути від'ємним")
+        # Присвоєння валідного значення приватному полю
+        self.__age = value
 
 
 if __name__ == "__main__":
-    rect1 = Rectangle(5, 10)
-    rect2 = Rectangle(3, 20)
-    rect3 = Rectangle(5, 10)
-    print(f"Площа прямокутників: {rect1.area()}, {rect2.area()}, {rect3.area()}")
-    print(rect1 == rect3)  # True: площі рівні
-    print(rect1 != rect2)  # True: площі не рівні
-    print(rect1 < rect2)  # True: площа rect1  менша, ніж у rect2
-    print(rect1 <= rect3)  # True: площі рівні, тому rect1 <= rect3
-    print(rect1 > rect2)  # False: площа rect1 менша, ніж у rect2
-    print(rect1 >= rect3)  # True: площі рівні, тому rect1 >= rect3
+    person = Person(-10)
+    print(person.age)
 
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Person:
+    def __init__(self, name: str, age: int, is_active: bool, is_admin: bool):
+        self.name = name
+        self.age = age
+        self._is_active = None
+        self.__is_admin = None
+        self._is_active = is_active
+        self.__is_admin = is_admin
 
-    def __eq__(self, other):
-        if not isinstance(other, Point):
-            return NotImplemented
-        return self.x == other.x and self.y == other.y
+    @property
+    def is_active(self):
+        return self._is_active
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+    @is_active.setter
+    def is_active(self, value: bool):
+        # Тут можна додати будь-яку логіку перевірки або обробки
+        self._is_active = value
 
-    def __lt__(self, other):
-        if not isinstance(other, Point):
-            return NotImplemented
-        return self.x < other.x and self.y < other.y
+    @property
+    def is_admin(self):
+        return self.__is_admin
 
-    def __gt__(self, other):
-        if not isinstance(other, Point):
-            return NotImplemented
-        return self.x > other.x and self.y > other.y
+    @is_admin.setter
+    def is_admin(self, value: bool):
+        # Тут можна додати будь-яку логіку перевірки або обробки
+        self.__is_admin = value
 
-    def __le__(self, other):
-        if not isinstance(other, Point):
-            return NotImplemented
-        return self.x <= other.x and self.y <= other.y
-
-    def __ge__(self, other):
-        if not isinstance(other, Point):
-            return NotImplemented
-        return self.x >= other.x and self.y >= other.y
+    def greeting(self):
+        return f"Hi {self.name}"
 
 
 if __name__ == "__main__":
-    print(Point(0, 0) == Point(0, 0))  # True
-    print(Point(0, 0) != Point(0, 0))  # False
-    print(Point(0, 0) < Point(1, 0))  # False
-    print(Point(0, 0) > Point(0, 1))  # False
-    print(Point(0, 2) >= Point(0, 1))  # True
-    print(Point(0, 0) <= Point(0, 0))  # True
+    p = Person("Boris", 34, True, False)
+    print(p.is_admin)  # Використовуємо геттер
+    p.is_admin = True  # Використовуємо сеттер
+    print(p.is_admin)
