51354468f478e20ef4705849feb4720f6b97db30
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-16 23:10:08 +0300

    Feat files

--- main.py
+++ main.py
@@ -1,44 +1,18 @@
-from contextlib import contextmanager
+expenses = {"hotel": 150, "breakfast": 30, "taxi": 15, "lunch": 20}
+
+file_name = "expenses.txt"
+with open(file_name, "w") as fh:
+    for key, value in expenses.items():
+        fh.write(f"{key}|{value}\n")
 
 
-@contextmanager
-def file_manager(filename, mode="w", encoding="utf-8"):
-    print("Відкриваємо файл", filename)
-    file = open(filename, mode, encoding=encoding)
-    try:
-        yield file
-    finally:
-        print("Закриваємо файл", filename)
-        file.close()
-        print("Завершення блоку with")
+file_name = "expenses.txt"
+expenses = {}
 
+with open(file_name, "r") as fh:
+    raw_expenses = fh.readlines()
+    for line in raw_expenses:
+        key, value = line.split("|")
+        expenses[key] = int(value)
 
-if __name__ == "__main__":
-    with file_manager("new_file.txt") as f:
-        f.write("Hello world!\n")
-        f.write("The end\n")
-
-
-from contextlib import contextmanager
-from datetime import datetime
-
-
-@contextmanager
-def managed_resource(*args, **kwargs):
-    log = ""
-    timestamp = datetime.now().timestamp()
-    msg = f"{timestamp:<20}|{args[0]:^15}| open \n"
-    log += msg
-    file_handler = open(*args, **kwargs)
-    try:
-        yield file_handler
-    finally:
-        diff = datetime.now().timestamp() - timestamp
-        msg = f"{timestamp:<20}|{args[0]:^15}| closed {round(diff, 6):>15}s \n"
-        log += msg
-        file_handler.close()
-        print(log)
-
-
-with managed_resource("new_file.txt", "r") as f:
-    print(f.read())
+print(expenses)
