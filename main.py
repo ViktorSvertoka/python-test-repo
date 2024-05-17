4911e345ad67171e5dc1b79e66213e88e7cb2aab
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-05-17 20:58:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-expenses = {"hotel": 150, "breakfast": 30, "taxi": 15, "lunch": 20}
 
-file_name = "expenses.txt"
-with open(file_name, "w") as fh:
-    for key, value in expenses.items():
-        fh.write(f"{key}|{value}\n")
-
-
-file_name = "expenses.txt"
-expenses = {}
-
-with open(file_name, "r") as fh:
-    raw_expenses = fh.readlines()
-    for line in raw_expenses:
-        key, value = line.split("|")
-        expenses[key] = int(value)
-
-print(expenses)
