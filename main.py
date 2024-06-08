dd6bb3636d1c6d0403a766335b3b398220ee788b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-08 12:32:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ fig, ax = plt.subplots()
 ax.spines[["left", "bottom"]].set_position(("data", 0))
 
 
-# Сховаємо верхню та праву лінію
+# Сховати верхню та праву лінію
 ax.spines[["top", "right"]].set_visible(False)
 
 
@@ -19,16 +19,22 @@ ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), clip_on=False)
 ax.plot(0, 1, "^k", transform=ax.get_xaxis_transform(), clip_on=False)
 
 
-# Сформуємо ряд значень x. 100 елементів від -10 до 10
-x = np.linspace(-10, 10.0, 100)
-
-
 # Додамо проміжні лінії
 ax.grid(True, linestyle="-.")
 
 
-# Сформуємо функцію y = x**2+2
-ax.plot(x, x**2 + 2)
+# Сформуємо ряд значень x. 100 елементів від -5 до 5
+x = np.linspace(-5, 5, 100, False)
+
+
+# Задаємо 2 частини функціональної залежності
+a = np.where(x > 3, 2 * x - 1, np.nan)
+b = np.where((x <= 3), 4, np.nan)
+
+
+# Генеруємо 2 графіки однакового кольору
+ax.plot(x, a, color="blue")
+ax.plot(x, b, color="blue")
 
 
 # Запускаємо малювання графіка
