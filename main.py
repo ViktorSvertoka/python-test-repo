913fb2441bb2e225f9793625b5c333b833f623ed
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-08 12:50:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,54 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Приклад 1: Квадратична функція
-def quadratic_function(x):
-    return x**2
+# Функція для побудови графіка періодичної функції
+def plot_periodic_function(func, period, title):
+    x_values = np.linspace(0, 3 * period, 1000)
+    y_values = func(x_values)
+
+    plt.plot(x_values, y_values)
+    plt.title(title)
+    plt.legend()
+
+
+# Приклад 1: Синус
+plt.subplot(2, 2, 1)
+plot_periodic_function(np.sin, 2 * np.pi, "Синус")
 
 
 # Приклад 2: Косинус
-def cosine_function(x):
-    return np.cos(x)
+plt.subplot(2, 2, 2)
+plot_periodic_function(np.cos, 2 * np.pi, "Косинус")
 
 
-# Приклад 3: Модуль x
-def absolute_value_function(x):
-    return np.abs(x)
-
-
-# Значення x для побудови графіка
-x_values = np.linspace(-5, 5, 100)
-
-
-# Графіки парних функцій
-plt.figure(figsize=(12, 4))
-
-
-plt.subplot(1, 3, 1)
-plt.plot(x_values, quadratic_function(x_values), label=r"$f(x) = x^2$")
-plt.plot(
-    x_values, quadratic_function(-x_values), linestyle="--", label=r"$f(-x) = x^2$"
-)
-plt.title("Квадратична функція")
-plt.legend()
-
-
-plt.subplot(1, 3, 2)
-plt.plot(x_values, cosine_function(x_values), label=r"$f(x) = \cos(x)$")
-plt.plot(
-    x_values, cosine_function(-x_values), linestyle="--", label=r"$f(-x) = \cos(x)$"
-)
-plt.title("Косинус")
-plt.legend()
-
-
-plt.subplot(1, 3, 3)
-plt.plot(x_values, absolute_value_function(x_values), label=r"$f(x) = |x|$")
-plt.plot(
-    x_values, absolute_value_function(-x_values), linestyle="--", label=r"$f(-x) = |x|$"
-)
-plt.title("Модуль x")
-plt.legend()
+# Приклад 3: Тангенс
+plt.subplot(2, 2, 3)
+plot_periodic_function(np.tan, np.pi, "Тангенс")
 
 
 plt.tight_layout()
