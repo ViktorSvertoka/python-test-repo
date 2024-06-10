57f35eb0990d636d9e93d58c418eda8980662d65
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-10 23:39:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import itertools
 
-val = [1, 2, 3, 4]
-perm_set = itertools.permutations(val, 2)
-n = 0
-for elem in perm_set:
-    print(elem)
-    n += 1
-print(n)
-
-
-import itertools
-
-val = [1, 2, 3, 4]
-perm_set = itertools.permutations(val)
-n = 0
-for elem in perm_set:
-    print(elem)
-    n += 1
-print(n)
-
-
-import itertools
-
-val = [1, 2, 3, 4]
-com_set = itertools.combinations(val, 2)
+val = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+com_set = itertools.combinations(val, 5)
 n = 0
 for elem in com_set:
-    print(elem)
+    # print(elem)
     n += 1
-print(n)
+C59 = n
+print(C59)
+val = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+com_set = itertools.combinations(val, 6)
+n = 0
+for elem in com_set:
+    # print(elem)
+    n += 1
+C610 = n
+print(C610)
+P = C59 / C610
+print(P)
+
+
+import itertools
+
+val = [1, 2, 3, 4, 5, 6, 7, 8]
+com_set = itertools.combinations(val, 4)
+n = 0
+for elem in com_set:
+    # print(elem)
+    n += 1
+C48 = n
+print(C48)
+val = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+com_set = itertools.combinations(val, 6)
+n = 0
+for elem in com_set:
+    # print(elem)
+    n += 1
+C610 = n
+print(C610)
+P = C48 / C610
+print(P)
