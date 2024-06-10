69f5c2b8804d9f9fa992e27a81539a5a6be8b8d0
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-10 22:59:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,25 @@ for elem in perm_set:
     print(elem)
     n += 1
 print(n)
+
+
+import itertools
+
+val = [1, 2, 3, 4]
+perm_set = itertools.permutations(val)
+n = 0
+for elem in perm_set:
+    print(elem)
+    n += 1
+print(n)
+
+
+import itertools
+
+val = [1, 2, 3, 4]
+com_set = itertools.combinations(val, 2)
+n = 0
+for elem in com_set:
+    print(elem)
+    n += 1
+print(n)
