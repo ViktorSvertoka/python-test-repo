3fd38caad8b44e2f8a5d196388fd189413883ad9
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-12 22:38:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 
-# make data
-n = [0.0016, 0.0256, 0.1536, 0.4096, 0.4096]
-
-
-# plot
-fig, ax = plt.subplots()
-
-
-ax.plot(n, linewidth=3.0)
-
-
-ax.set(
-    xlim=(0, 4), xticks=np.arange(0, 4.1), ylim=(0, 0.5), yticks=np.arange(0, 0.5, 0.1)
-)
+n = [0.0016, 0.0272, 0.1808, 0.5904, 1]
+print(n)
+plt.stairs(n, fill=True, label="Distribution")
 plt.grid(True)
 plt.show()
