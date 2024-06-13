303a036fc5808ed1f122bba57302830ea66bc37e
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-13 23:41:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
+import numpy as np
+import statistics as st
 import math
 
-x = [113, 114, 106, 108, 120, 104, 116, 112, 110, 118, 103, 120]
-y = [10, 15, 20, 15, 24, 11, 20, 20, 18, 24, 14, 27]
+x1 = [6, 7, 4, 5, 4, 5, 3, 6, 7, 3, 7, 3, 5, 4, 4, 3, 5, 4]
+x2 = [4, 4, 5, 4, 1, 5, 5, 3, 3, 6, 2, 3, 4, 3, 7, 5, 3, 2, 4, 5]
 
 
-# Обраховуємо суму та середнє значення
-sum_x = sum(x)
-avg_x = sum(x) / len(x)
-sum_y = sum(y)
-avg_y = sum(y) / len(y)
+x1_avg = np.average(x1)
+x2_avg = np.average(x2)
 
 
-# Обраховуємо ряд, який є різницею між первинним рядом та середнім значенням ряду
-x_minus_avg = []
-y_minus_avg = []
-for i in range(len(x)):
-    x_minus_avg.append(x[i] - avg_x)
-    y_minus_avg.append(y[i] - avg_y)
+x1_disp = st.variance(x1)
+x2_disp = st.variance(x2)
 
 
-# Обраховуємо квадрати різниці значень ряду та її середніх, і добуток різниць значень ряду та їх середніх різних змінних
-xma2 = []
-yma2 = []
-xma_yma = []
-for i in range(len(x_minus_avg)):
-    xma2.append(x_minus_avg[i] ** 2)
-    yma2.append(y_minus_avg[i] ** 2)
-    xma_yma.append(x_minus_avg[i] * y_minus_avg[i])
+n1 = len(x1)
+n2 = len(x2)
 
 
-# Обраховуємо суми отриманих рядів
-sum_xma2 = sum(xma2)
-sum_yma2 = sum(yma2)
-sum_xma_yma = sum(xma_yma)
+print(f"Середні значення: {x1_avg} {x2_avg}")
+print(f"Дисперсії: {x1_disp} {x2_disp}")
+print(f"Розмір вибірки: {n1} {n2}")
 
 
-# Обраховуємо критерій кореляції Пірсона
-r_x_y = sum_xma_yma / (math.sqrt(sum_xma2 * sum_yma2))
-print(r_x_y)
+t = (x1_avg - x2_avg) / (
+    math.sqrt(
+        (((n1 - 1) * x1_disp + (n2 - 1) * x2_disp)) / (n1 + n2 - 2) * (1 / n1 + 1 / n2)
+    )
+)
+print(t)
+# t критичне для 38 ступенів свободи 2,024
+# Визначаємо дисперсію
