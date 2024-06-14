d88f1fd9c56112585c5a55c4e84807e6d0a74fce
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-14 12:04:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 import numpy as np
-import statistics as st
-import math
-
-x1 = [6, 7, 4, 5, 4, 5, 3, 6, 7, 3, 7, 3, 5, 4, 4, 3, 5, 4]
-x2 = [4, 4, 5, 4, 1, 5, 5, 3, 3, 6, 2, 3, 4, 3, 7, 5, 3, 2, 4, 5]
+import matplotlib.pyplot as plt
+import random
 
 
-x1_avg = np.average(x1)
-x2_avg = np.average(x2)
+N = len(value_data.Q106)  # розмір вибірки
+X = value_data.Q106
 
 
-x1_disp = st.variance(x1)
-x2_disp = st.variance(x2)
+# формується 2 масиви: n - кількість елементів, що попали в інтервал, x - масив меж інтервалів
+print(np.histogram(X, bins=13))
+n, x = np.histogram(X, bins=13)
 
 
-n1 = len(x1)
-n2 = len(x2)
+# масив початків інтервалів (прибрали останнє, найбільше значення)
+xmin = x[:-1]
 
 
-print(f"Середні значення: {x1_avg} {x2_avg}")
-print(f"Дисперсії: {x1_disp} {x2_disp}")
-print(f"Розмір вибірки: {n1} {n2}")
+# ширина інтервалу (різниця двох послідовних стовпчиків).
+dx = x[1] - x[0]
 
 
-t = (x1_avg - x2_avg) / (
-    math.sqrt(
-        (((n1 - 1) * x1_disp + (n2 - 1) * x2_disp)) / (n1 + n2 - 2) * (1 / n1 + 1 / n2)
-    )
-)
-print(t)
-# t критичне для 38 ступенів свободи 2,024
-# Визначаємо дисперсію
+# емпірична приведена частота: частка від кількості елементів в інтервалі та загальної кількості елементів.
+y = n / N
+
+
+# масив центрів інтервалів
+xmid = xmin + dx / 2
+# виводимо емпіричну гістограму приведених частот
+plt.bar(xmid, y, width=dx, color="y")
+
+
+# Виводимо підписи на осях
+plt.xlabel("x")
+plt.ylabel("Частота (ймовірність)")
+
+
+# Малюємо сітку та гістограму.
+plt.grid()
+plt.show()
