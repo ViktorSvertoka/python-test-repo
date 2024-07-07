043b69bf0f8f01a6d53e0759d49d64e37ce20aa7
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-07-07 11:12:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,30 @@
-my_dict = {}
-my_dict["key1"] = "value1"
-my_dict["key2"] = "value2"
-print(my_dict)  # {'key1': 'value1', 'key2': 'value2'}
+import networkx as nx
+import matplotlib.pyplot as plt
 
+# Створюємо порожній граф
+G = nx.Graph()
 
-del my_dict["key1"]
-print(my_dict)  # {'key2': 'value2'}
+# Додаємо вершини
+G.add_node("A")
+G.add_node("B")
+G.add_node("C")
 
+# Додаємо з'єднання
+G.add_edge("A", "B")
+G.add_edge("B", "C")
 
-value = my_dict.get("key2")
-print(value)  # value2
+# Задаємо розташування вершин
+positions = {"A": (0, 0.5), "B": (1, 0.5), "C": (2, 0.5)}
 
-
-my_dict["key2"] = "new value"
-print(my_dict)  # {'key2': 'new value'}
-
-
-key1 = "example"
-hash_value1 = hash(key1)
-print(hash_value1)  # -4049593035154628903
-
-key2 = "another example"
-hash_value2 = hash(key2)
-print(hash_value2)  # 5109324920784817575
-
-
-import hashlib
-
-data = "example data"
-
-# Використання SHA-256
-hashed_data = hashlib.sha256(data.encode())
-print(
-    hashed_data.hexdigest()
-)  # 44752f37272e944fd2c913a35342eaccdd1aaf189bae50676b301ab213fc5061
-
-
-import hashlib
-import os
-
-
-def get_hash(path):
-    """Повертає хеш-значення для файлу."""
-    with open(path, "rb") as file:
-        bytes = file.read()
-        readable_hash = hashlib.sha256(bytes).hexdigest()
-        return readable_hash
-
-
-def find_duplicates(directory):
-    """Шукає дублікати в директорії."""
-    hashes = {}
-    duplicates = []
-    for dirpath, dirnames, filenames in os.walk(directory):
-        for filename in filenames:
-            path = os.path.join(dirpath, filename)
-            file_hash = get_hash(path)
-            if file_hash not in hashes:
-                hashes[file_hash] = path
-            else:
-                duplicates.append((path, hashes[file_hash]))
-    return duplicates
-
-
-# Пошук дублікатів у заданій директорії
-duplicates = find_duplicates("/path/to/directory")
-for duplicate in duplicates:
-    print(f"Дублікатні файли: {duplicate[0]} та {duplicate[1]}")
+# Малюємо граф
+plt.figure(figsize=(6, 6))
+nx.draw_networkx(
+    G,
+    pos=positions,
+    with_labels=True,
+    font_weight="bold",
+    node_color="lightblue",
+    edge_color="gray",
+)
+plt.axis("off")
+plt.show()
