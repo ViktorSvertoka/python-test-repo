ab9cca434a32233f0fb0590da1d8933083444ebc
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-07-10 22:48:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
-def factorial(n):
-    if n == 0:  # базовий випадок
-        return 1
-    else:
-        return n * factorial(n - 1)  # рекурсивний випадок
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 
-print(factorial(5))  # виведе 120
+def draw_triangle(vertices, ax):
+    triangle = patches.Polygon(vertices, fill=False, edgecolor="black")
+    ax.add_patch(triangle)
 
 
-def fibonacci(n):
-    if n <= 1:  # базовий випадок
-        return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)  # рекурсивний випадок
+def midpoint(point1, point2):
+    return [(point1[0] + point2[0]) / 2, (point1[1] + point2[1]) / 2]
 
 
-print(fibonacci(10))  # виведе 55
+def sierpinski(vertices, level, ax):
+    draw_triangle(vertices, ax)
+    if level > 0:
+        sierpinski(
+            [
+                vertices[0],
+                midpoint(vertices[0], vertices[1]),
+                midpoint(vertices[0], vertices[2]),
+            ],
+            level - 1,
+            ax,
+        )
+        sierpinski(
+            [
+                vertices[1],
+                midpoint(vertices[0], vertices[1]),
+                midpoint(vertices[1], vertices[2]),
+            ],
+            level - 1,
+            ax,
+        )
+        sierpinski(
+            [
+                vertices[2],
+                midpoint(vertices[2], vertices[1]),
+                midpoint(vertices[0], vertices[2]),
+            ],
+            level - 1,
+            ax,
+        )
 
 
-def stupid_recursion():
-    stupid_recursion()
+def main():
+    fig, ax = plt.subplots()
+    ax.set_aspect("equal")
+    ax.set_axis_off()
+    vertices = [[0, 0], [0.5, 0.75], [1, 0]]
+    sierpinski(vertices, 3, ax)
+    plt.show()
 
 
-stupid_recursion()
+main()
